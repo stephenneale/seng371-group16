6266788a061d1038a95079c9fc4941c2400203f4
Matt Lebl <mlebl@uvic.ca>
2023-03-22 20:34:09 -0600

    Add comments and percentages to grades endpoint

--- as-developers/backend/src/main/kotlin/ca/uvic/dimspace/controller/GradeController.kt
+++ as-developers/backend/src/main/kotlin/ca/uvic/dimspace/controller/GradeController.kt
@@ -19,37 +19,51 @@ class GradeController {
 				GradeDto(
 					"Assignment 1",
 					22.5,
-					25.0
+					25.0,
+					22.5/25.0,
+					null,
 				),
 				GradeDto(
 					"Assignment 2",
 					24.0,
 					25.0,
+					24.0/25.0,
+					"Excellent work!",
 				),
 				GradeDto(
 					"Assignment 3",
 					null,
 					25.0,
+					null,
+					null,
 				),
 				GradeDto(
 					"Assignment 4",
 					null,
 					25.0,
+					null,
+					null,
 				),
 				GradeDto(
 					"Midterm",
 					48.5,
 					50.0,
+					48.5/50.0,
+					"Amazing!",
 				),
 				GradeDto(
 					"Quiz 1",
 					5.0,
 					5.0,
+					1.0,
+					null,
 				),
 				GradeDto(
 					"Quiz 2",
 					null,
-					5.0
+					5.0,
+					null,
+					null,
 				),
 			)
 		)
--- as-developers/backend/src/main/kotlin/ca/uvic/dimspace/dto/GradeDto.kt
+++ as-developers/backend/src/main/kotlin/ca/uvic/dimspace/dto/GradeDto.kt
@@ -4,4 +4,6 @@ data class GradeDto(
 	val itemName: String,
 	val markNumerator: Double?,
 	val markDenominator: Double,
+	val percentage: Double?,
+	val comment: String?,
 )
